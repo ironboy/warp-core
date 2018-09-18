d7518abab6608c07d1b1ee92481f89dc4154b6f2
Thomas Frank <t@clientsmile.se>
2018-09-18 17:27:27 +0200

    again

--- mod-webpack.config.dev.js
+++ mod-webpack.config.dev.js
@@ -3,5 +3,5 @@ const config = require('./org-webpack.config.dev.js');
 const path = require('path');
 const basePath = __dirname.split(path.sep).slice(0,-1).join(path.sep);
 module.exports = (require(path.join(
-  basePath, 'node_modules', 'warp-core', 'mod-webpack-config-dev-module.js'
+  basePath, 'node_modules', 'react-warp-core', 'mod-webpack-config-dev-module.js'
 )))(config, basePath);
