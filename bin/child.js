06d0e7a1be71aa785e3c6025c00fc098b445cbd5
Thomas Frank <t@clientsmile.se>
2018-09-20 17:31:49 +0200

    alternate npm install way

--- bin/child.js
+++ bin/child.js
@@ -3,11 +3,26 @@ const fs = require('fs');
 const exec = (require('child_process')).execSync;
 const ncp = require('ncp');
 const rimraf = require('rimraf');
+const npm = require('npm-cmd');
 
 // Paths (mostly set in installReact)
 const paths = {
   myBase: path.join(__dirname, '..')
 };
+
+// a function to run npm installs, run async
+function npmInstall(...packages){
+  return new Promise((res, rej) => {
+    npm.install(...packages, {save: true, cwd: paths.projectFolder}, (err)=>{
+      if(err){
+        res({error: JSON.stringify(error)})
+      }
+      else {
+        res({success: true});
+      }
+    });
+  });
+}
 
 // Functions to run
 // (will be added below)
@@ -64,14 +79,12 @@ tasksFuncs['Ejecting the project'] = () => {
 
 }
 
-tasksFuncs['Installing Warp-Core (extra webpack loaders etc)'] = () => {
-  exec('npm install react-warp-core', {windowsHide: true, cwd: paths.projectFolder});
-  return {success: true};
+tasksFuncs['Installing Warp-Core (extra webpack loaders etc)'] = async () => {
+  return await npmInstall(['react-warp-core']);
 }
 
-tasksFuncs['Adding SCSS/SASS support'] = () => {
-  exec('npm install node-sass sass-loader', {windowsHide: true, cwd: paths.projectFolder});
-  return {success: true};
+tasksFuncs['Adding SCSS/SASS support'] = async () => {
+  return await npmInstall(['node-sass', 'sass-loader']);
 }
 
 tasksFuncs['Patching the dev config to use new webpack loaders'] = () => {
@@ -91,9 +104,8 @@ tasksFuncs['Patching the dev config to use new webpack loaders'] = () => {
   return {success: true};
 }
 
-tasksFuncs['Installing the web server Express'] = () => {
-  exec('npm install express', {windowsHide: true, cwd: paths.projectFolder});
-  return {success: true};
+tasksFuncs['Installing the web server Express'] = async () => {
+  return await npmInstall(['express']);
 }
 
 tasksFuncs['Adding proxy settings for Express'] = () => {
@@ -124,9 +136,8 @@ tasksFuncs['Adding a folder for the Express app'] = () => {
   return {success: true}
 }
 
-tasksFuncs['Installing react-router-dom'] = () => {
-  exec('npm install react-router-dom@4.3.1', {windowsHide: true, cwd: paths.projectFolder});
-  return {success: true};
+tasksFuncs['Installing react-router-dom'] = async () => {
+  return await npmInstall(['react-router-dom@4.3.1']);
 }
 
 tasksFuncs['Removing the src folder from create react app'] = () => {
