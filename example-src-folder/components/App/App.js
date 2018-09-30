9421673ae4782b0ada12c4aa0911639cdb85cc6b
Thomas Frank <t@clientsmile.se>
2018-09-30 22:32:41 +0200

    changing fade-in

--- example-src-folder/components/App/App.js
+++ example-src-folder/components/App/App.js
@@ -10,9 +10,9 @@ import './App.scss';
     // avoid FOUC by waiting for styles to load
     // and then fade in the app
     while(this.style.opacity < 1){
-      await sleep(20);
+      await sleep(60);
       let ok = $('body').css('font-family').length > 10;
-      this.style.opacity += ok ? .03 : 0;
+      this.style.opacity += ok ? .1 : 0;
     }
 
     // no focusing of buttons when you click them
